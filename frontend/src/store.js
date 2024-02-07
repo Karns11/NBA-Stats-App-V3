2e2eca14779533a27c880bec551006df098c72b8
nathan karns <Nathan k>
2024-02-06 22:10:35 -0500

    last change for today

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
-import authreducer from "./slices/authSlice";
+import authReducer from "./slices/authSlice";
 import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authreducer, // add this line
+    auth: authReducer, // add this line
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
